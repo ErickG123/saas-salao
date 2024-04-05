328c53bb6a4925ce74083b5d866e10d817cec522
Erick Gabriel <erickgabrielalves@hotmail.com>
2024-04-05 19:39:43 -0300

    fix: remoção pagina sem uso e texto

--- web/src/app/users/verify-otp/page.tsx
+++ web/src/app/users/verify-otp/page.tsx
@@ -35,12 +35,12 @@ export default function VerifyOtp() {
       <div className="w-1/3 border shadow-md rounded-md p-6">
         <h1 className="flex justify-center items-center text-5xl font-bold">SaaS</h1>
         <p className="text-center my-4 text-sm">
-          Digite seu e-mail para receber o link de redefinição de senha.
+          Informe o código OTP para ativar a sua conta.
         </p>
         <div className="w-full flex flex-col gap-2">
           <input className="border border-gray-300 rounded-md p-2.5 outline-none" type="text" placeholder="Código OTP" {...register("otpCode", { required: true })} />
         </div>
-        <button className="w-full p-2.5 mt-2 border rounded-md bg-slate-950 hover:bg-slate-800 text-white font-semibold">Enviar E-mail</button>
+        <button className="w-full p-2.5 mt-2 border rounded-md bg-slate-950 hover:bg-slate-800 text-white font-semibold">Ativar Conta</button>
       </div>
     </form>
   );
